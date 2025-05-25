234199a2abac5364672c8f605ab0251fbdb0e3d0
Trusha Avaiya <trushaavaiya@gmail.com>
2025-05-25 10:37:49 +0530

    Image ,Text and Button code

--- src/Screen1.js
+++ src/Screen1.js
@@ -12,13 +12,34 @@ import {
 
 export default function Screen1() {
   return (
-        <View>
-            
+        <View style={styles.container}>
+        <StatusBar barStyle="dark-content" backgroundColor="#fff" />
+
+      
+      <Image
+        source={require('../assets/messenger-icon.png')} 
+        style={styles.logo}
+      />
+
+      
+      <Text style={styles.title}>Text{'\n'}<Text style={styles.bold}>Messenger</Text></Text>
+
+      
+      <Text style={styles.subtitle}>
+        Let’s Connect with your friends and family with this{'\n'}
+        fastest and instant messaging app
+      </Text>
+
+      
+      <TouchableOpacity style={styles.button}>
+        <Text style={styles.buttonText}>Give Me Permission</Text>
+      </TouchableOpacity>
+
         </View>
   );
 }
 
-const style = style.create({
+const styles = StyleSheet.create({
 
 
 });
