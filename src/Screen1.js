f5ee10df6af751024f8f40b74bf267548e659446
Trusha Avaiya <trushaavaiya@gmail.com>
2025-05-25 10:38:13 +0530

    css

--- src/Screen1.js
+++ src/Screen1.js
@@ -40,6 +40,45 @@ export default function Screen1() {
 }
 
 const styles = StyleSheet.create({
-
+container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+    padding: 20,
+  },
+  logo: {
+    width: 120,
+    height: 120,
+    resizeMode: 'contain',
+    marginBottom: 30,
+  },
+  title: {
+    fontSize: 28,
+    textAlign: 'center',
+    color: '#333',
+  },
+  bold: {
+    fontWeight: 'bold',
+  },
+  subtitle: {
+    fontSize: 14,
+    color: '#555',
+    textAlign: 'center',
+    marginTop: 10,
+    marginBottom: 40,
+  },
+  button: {
+    backgroundColor: 'linear-gradient(90deg, #9F6CF2 0%, #3F2B96 100%)',
+    paddingVertical: 14,
+    paddingHorizontal: 30,
+    borderRadius: 30,
+    backgroundColor: '#9F6CF2', 
+    elevation: 4,
+  },
+  buttonText: {
+    color: '#fff',
+    fontWeight: 'bold',
+  },
 
 });
