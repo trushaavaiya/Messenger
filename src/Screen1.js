a818919f8ecba3f5f49c303abd7be4c29e3ce801
Trusha Avaiya <trushaavaiya@gmail.com>
2025-05-25 10:36:20 +0530

    Basic statement

---
+++ src/Screen1.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import {
+  View,
+  Text,
+  Image,
+  StyleSheet,
+  TouchableOpacity,
+  StatusBar,
+  Dimensions,
+} from 'react-native';
+
+
+export default function Screen1() {
+  return (
+        <View>
+            
+        </View>
+  );
+}
+
+const style = style.create({
+
+
+});
