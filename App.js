55848416dfff3fb2b0b7a62a40491fdf1e42058e
Trusha Avaiya <trushaavaiya@gmail.com>
2025-05-25 10:32:04 +0530

    import statement for Screen1

---
+++ App.js
@@ -0,0 +1,59 @@
+
+
+import React from 'react';
+import {
+  ScrollView,
+  StatusBar,
+  StyleSheet,
+  Text,
+  useColorScheme,
+  View,
+} from 'react-native';
+
+import {
+  Colors,
+  DebugInstructions,
+  Header,
+  LearnMoreLinks,
+  ReloadInstructions,
+} from 'react-native/Libraries/NewAppScreen';
+import Screen1 from './src/Screen1';
+
+
+
+function App() {
+
+  return (
+    <View style={{flex:1}}>
+      <StatusBar
+        barStyle={'light-content'}
+      />
+      <Screen1/>
+      
+      
+       
+      
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  sectionContainer: {
+    marginTop: 32,
+    paddingHorizontal: 24,
+  },
+  sectionTitle: {
+    fontSize: 24,
+    fontWeight: '600',
+  },
+  sectionDescription: {
+    marginTop: 8,
+    fontSize: 18,
+    fontWeight: '400',
+  },
+  highlight: {
+    fontWeight: '700',
+  },
+});
+
+export default App;
