3db85e9573d93a90bc4eba6ab90a346d9f13b4b1
Trusha Avaiya <trushaavaiya@gmail.com>
2025-05-29 12:09:07 +0530

    import statemnt for screen 24

--- App.js
+++ App.js
@@ -41,7 +41,8 @@ import {
 // import Screen20 from './src/Screen20';
 // import Screen21 from './src/Screen21';
 // import Screen22 from './src/Screen22';
-import Screen23 from './src/Screen23';
+// import Screen23 from './src/Screen23';
+import Screen24 from './src/Screen24';
 
 function App() {
   return (
@@ -71,7 +72,8 @@ function App() {
        {/* <Screen19/>  */}
        {/* <Screen20/>  */}
        {/* <Screen21/>  */}
-       <Screen23/> 
+       {/* <Screen23/>  */}
+       <Screen24/> 
       
 
     </View>
