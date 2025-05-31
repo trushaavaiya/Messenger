32da8a221c6e8058d3176109dbf653ed62c649c3
Trusha Avaiya <trushaavaiya@gmail.com>
2025-05-31 10:32:32 +0530

    Added color for screen1

---
+++ src/constants/colors.js
@@ -0,0 +1,18 @@
+const Colors = {
+  primary: '#9F6CF2',
+  background: '#fff',
+  text: '#333',
+  subtitle: '#555',
+  buttonText: '#fff',
+
+  circle: '#caa1ff',
+  circleSmall: '#d99bff',
+  triangle: '#b264ff',
+  triangleOrange: '#ff9900',
+  circleGradient: '#fdbb9c',
+  zigzag: '#ff3b6c',
+  zigzagSmall: '#8b5cf6',
+  orangeRing: '#ff7e00',
+};
+
+export default Colors;
