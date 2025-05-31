92a43f6c442fef49ca187e9f88e4ba234793ce18
Trusha Avaiya <trushaavaiya@gmail.com>
2025-05-31 11:08:10 +0530

    added color for screen 3

--- src/constants/fonts.js
+++ src/constants/fonts.js
@@ -3,6 +3,12 @@ const Fonts = {
   bold: 'System',
   titleSize: 28,
   subtitleSize: 14,
+
+  headerFontSize: 20,
+  headerFontWeight: '600',
+  contactNameSize: 16,
+  contactNameWeight: '600',
+  contactPhoneSize: 14,
 };
 
 export default Fonts;
